3f2b8b451c4fcef04c9a3a844d0820ce91e101be
suttonj07 <suttonj07@gmail.com>
2024-08-03 14:05:14 -0400

    edits

--- practice.py
+++ practice.py
@@ -10,13 +10,10 @@ print(max_num(15,30,2))
 # Write a Python function called mult_list()  to multiply all the numbers in a list.
 
 def mult_list(lst):
-  #if empty list, return 0
   if len(lst) == 0:
     return 0
-  #product starts with first element of list
   prod = lst[0]
 
-  #go through list elements and multiply them together
   if len(lst) > 1:
     for i in lst[1:]:
       prod = prod * i
@@ -50,33 +47,28 @@ print(num_within(10,2,5))
 
 triangle = [[1],[1,1]]
 def pascal(n):
-  #base case
+ 
   if n < 1:
     print("invalid number of rows")
   elif n == 1:
     print(triangle[0])
   else:
     row_number = 2
-    #fill up correct number of rows in triangle
+    
     while len(triangle) < n:
       row = []
       row_prev = triangle[row_number - 1]
-      #create correct row, then add to triangle (this row will be 1 longer than row before it)
       length = len(row_prev)+1
       for i in range(length):
-        #first number is 1
         if i == 0:
           row.append(1)
-        #intermediate nunmbers get added from previous rows
         elif i > 0 and i < length-1:
           row.append(triangle[row_number-1][i-1]+triangle[row_number-1][i])
-        #last number is 1
         else:
           row.append(1)
       triangle.append(row)
       row_number += 1
 
-    #print triangle
     for row in triangle:
       print(row)
 
